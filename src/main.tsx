405e678b71f17ba00bb288a0567ca286a4bef8b0
Chuhj <chj9574@naver.com>
2023-06-27 23:12:24 +0900

    feat: 라우터 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import '@/styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
