4c084060c32d6e85876be7390c329c46e7fc7f14
Chuhj <chj9574@naver.com>
2023-06-27 23:09:56 +0900

    chore: tailwind spacing, borderRadius 커스텀

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
+const spacing = Array(101)
+  .fill()
+  .reduce((acc, _, index) => {
+    const value = index * 2;
+    acc[value] = `${value / 10}rem`;
+    return acc;
+  }, {});
+
+const borderRadius = Array(51)
+  .fill()
+  .reduce((acc, _, index) => {
+    const value = index * 2;
+    acc[value] = `${value / 10}rem`;
+    return acc;
+  }, {});
+
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    spacing,
+    borderRadius,
     extend: {},
   },
   plugins: [],
