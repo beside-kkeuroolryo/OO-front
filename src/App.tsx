fa20549fd64e60d69f4072a9de5e88022f5e9d5d
Chuhj <chj9574@naver.com>
2023-07-01 00:50:18 +0900

    feat: 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from '@/pages/Home';
+import Questions from '@/pages/Questions';
 
 function App() {
   return (
-    <div className="mx-auto max-w-[39rem]">
+    <div className="mx-auto h-full max-w-[39rem]">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="questions">
+          <Route path="self" element={<Questions />} />
+          <Route path="couple" element={<Questions />} />
+          <Route path="friends" element={<Questions />} />
+          <Route path="random" element={<Questions />} />
+          <Route path="custom" element={<Questions />} />
+          <Route path="result" element={<Questions />} />
+        </Route>
       </Routes>
     </div>
   );
