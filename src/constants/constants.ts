15964afc39aaec1f839af82a221ddf21f1018600
Chuhj <chj9574@naver.com>
2023-07-02 00:14:53 +0900

    chore: 카테고리 상수 선언

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,1 +1,7 @@
 export const QUESTIONS_COUNT = 15;
+
+export const SELF = 'self';
+export const COUPLE = 'couple';
+export const FRIEND = 'friend';
+export const RANDOM = 'random';
+export const USERMADE = 'usermade';
