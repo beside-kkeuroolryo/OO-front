b2c754adba8b7516faa9a043b10674e0d19b35ca
Chuhj <chj9574@naver.com>
2023-07-02 07:41:42 +0900

    fix: api 경로 수정

--- src/api/questions.ts
+++ src/api/questions.ts
@@ -3,28 +3,6 @@ import { AxiosError } from 'axios';
 import { QuestionType } from '@/types/questions';
 import axiosInstance from '@/api/config/axios';
 
-// export const useGetQuestionIds = (category?: string, idsState?: number[]) => {
-//   return useQuery<number[], AxiosError>(
-//     [category, 'ids'],
-//     async () => {
-//       const { data } = await axiosInstance.get(`/api/golrabas/category/${category}`);
-//       return data.data.questionIds;
-//     },
-//     { enabled: !idsState },
-//   );
-// };
-
-// export const useGetQuestion = (id?: number) => {
-//   return useQuery<QuestionType, AxiosError>(
-//     ['question', id],
-//     async () => {
-//       const { data } = await axiosInstance.get(`/api/golrabas/${id}`);
-//       return data.data;
-//     },
-//     { enabled: id !== undefined },
-//   );
-// };
-
 export const usePostResult = () => {
   return useMutation((body: { questionId?: number; choice?: '' | 'a' | 'b' }[]) => {
     return axiosInstance.post('/api/golrabas/result', { results: body });
@@ -32,12 +10,10 @@ export const usePostResult = () => {
 };
 
 export const useGetQuestionIds = (category?: string, idsState?: number[]) => {
-  category;
   return useQuery<number[], AxiosError>(
-    ['self', 'ids'],
+    [category, 'ids'],
     async () => {
-      console.log('ids fetch');
-      const { data } = await axiosInstance.get(`/category.json`);
+      const { data } = await axiosInstance.get(`/api/golrabas/category/${category}`);
       return data.data.questionIds;
     },
     { enabled: !idsState },
@@ -48,10 +24,15 @@ export const useGetQuestion = (id?: number) => {
   return useQuery<QuestionType, AxiosError>(
     ['question', id],
     async () => {
-      console.log('question fetch');
-      const { data } = await axiosInstance.get(`/question.json`);
+      const { data } = await axiosInstance.get(`/api/golrabas/${id}`);
       return data.data;
     },
     { enabled: id !== undefined },
   );
+};
+
+export const usePostQuestion = () => {
+  return useMutation((body: { content: string; choiceA: string; choiceB: string }) => {
+    return axiosInstance.post(`/api/golrabas/question`, body);
+  });
 };
