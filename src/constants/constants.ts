0d3cfac9ff95e5b474c4fe565e55a40ce134e816
Chuhj <chj9574@naver.com>
2023-07-02 05:15:41 +0900

    feat: 최대 길이 상수 추가

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -5,3 +5,7 @@ export const COUPLE = 'couple';
 export const FRIEND = 'friend';
 export const RANDOM = 'random';
 export const USERMADE = 'usermade';
+
+export const MAX_NICKNAME_LENGTH = 15;
+export const MAX_PASSWORD_LENGTH = 15;
+export const MAX_COMMENT_LENGTH = 100;
