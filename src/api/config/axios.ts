ef96e94ab79ebd36605d466a4a058fda5aa71cfe
Chuhj <chj9574@naver.com>
2023-07-02 13:55:11 +0900

    fix: withCredentials 위치 수정

--- src/api/config/axios.ts
+++ src/api/config/axios.ts
@@ -4,8 +4,8 @@ const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
-    withCredentials: true,
   },
+  withCredentials: true,
 });
 
 export default axiosInstance;
