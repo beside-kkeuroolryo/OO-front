f9841717f89155548a4ddb0ecf92fb9a1c7b2aa3
Chuhj <chj9574@naver.com>
2023-09-27 03:13:36 +0900

    feat(Result): 게임 결과 공유 기능 추가

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -10,6 +10,8 @@ import useCustomToast from '@/hooks/useCustomToast';
 import { USERMADE } from '@/constants/questions';
 import { usePostResult } from '@/api/questions';
 
+type ButtonsTuple = [React.FC<React.SVGProps<SVGSVGElement>>, string, () => void];
+
 export default function Result() {
   const navigate = useNavigate();
   const { state } = useLocation();
@@ -31,12 +33,58 @@ export default function Result() {
 
   const ids = idsState ? idsState : JSON.parse(idsQueryString ? idsQueryString : '[]');
 
+  const share = (data: ShareData) => {
+    try {
+      navigator.share(data);
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(
+          <div>
+            공유에 실패했습니다.
+            <br />
+            {error.message && error.message}
+          </div>,
+        );
+      }
+      copy();
+    }
+  };
+
+  const copy = async () => {
+    try {
+      await navigator.clipboard.writeText(shareUrl);
+      toast.success('클립보드에 복사되었습니다.');
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(
+          <div>
+            복사에 실패했습니다.
+            <br />
+            {error.message && error.message}
+          </div>,
+        );
+      }
+    }
+  };
+
   const handleGoHome = () => {
     navigate('/');
   };
 
   const handleRetry = () => {
     navigate(`/questions/${category}`, { state: idsState });
+  };
+
+  const handleShare = async () => {
+    const data: ShareData = {
+      title: '골라바 게임 완료!',
+      url: shareUrl,
+    };
+
+    if (navigator.share && navigator.canShare && navigator.canShare(data)) {
+      return share(data);
+    }
+    return copy();
   };
 
   useEffect(() => {
@@ -64,7 +112,7 @@ export default function Result() {
 
   return (
     <main className="h-full bg-dark text-primary">
-      <section aria-labelledby="finish" className="rounded-b-28 bg-white ">
+      <section aria-labelledby="finish" className="rounded-b-28 bg-white">
         <Navbar isResult className="px-24" />
         <div className="flex flex-col items-center gap-32 bg-[url('../assets/images/finish.svg')] bg-cover bg-top bg-no-repeat pb-[2.63rem] pt-12">
           <div className="flex flex-col gap-8">
@@ -74,22 +122,27 @@ export default function Result() {
             <div className="font-16 font-semibold text-secondary">선택한 결과를 확인해볼까요?</div>
           </div>
           <ul className="font-16 flex w-full justify-center gap-26 font-semibold">
-            {[
-              [Home, '메인으로'],
-              [Share, '공유하기'],
-              [Retry, '다시하기'],
-            ].map(([Icon, text]) => (
-              <li key={text as string}>
-                <button
-                  aria-label={text as string}
-                  type="button"
-                  className="mx-auto mb-8 flex h-[4.67rem] w-[4.67rem] items-center justify-center rounded-100 bg-dark text-cyan"
-                >
-                  <Icon aria-hidden={true} />
-                </button>
-                {text as string}
-              </li>
-            ))}
+            {(
+              [
+                [Home, '메인으로', handleGoHome],
+                [Share, '공유하기', handleShare],
+                [Retry, '다시하기', handleRetry],
+              ] as ButtonsTuple[]
+            ).map(([Icon, text, handler]) => {
+              return (
+                <li key={text}>
+                  <button
+                    aria-label={text}
+                    type="button"
+                    className="mx-auto mb-8 flex h-[4.67rem] w-[4.67rem] items-center justify-center rounded-100 bg-dark text-cyan"
+                    onClick={handler}
+                  >
+                    <Icon aria-hidden={true} />
+                  </button>
+                  {text}
+                </li>
+              );
+            })}
           </ul>
         </div>
       </section>
