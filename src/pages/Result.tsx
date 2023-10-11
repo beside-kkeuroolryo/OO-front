e2c7732952a851793d3aa0339150a9b69e9347c0
Chuhj <chj9574@naver.com>
2023-10-11 20:07:30 +0900

    chore: 불필요 코드 제거

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { ResultToPost, ResultToRender } from '@/pages/Questions';
